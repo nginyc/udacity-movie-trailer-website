55993eb71f06e4c655a4453f296544b5002ad320
Ngin Yun Chuan <yunchuann@gmail.com>
2018-05-29 08:14:00 +0800

    Make code PEP8 compliant

--- media.py
+++ media.py
@@ -1,13 +1,16 @@
 import webbrowser
 
+
 class Movie():
     '''
       Abstraction of a movie on the website
     '''
+
+    # Initializes a movie with a title, post image and trailer
     def __init__(self, title, poster_image_url, trailer_url):
-      self.title = title
-      self.poster_image_url = poster_image_url
-      self.trailer_youtube_url = trailer_url
+        self.title = title
+        self.poster_image_url = poster_image_url
+        self.trailer_youtube_url = trailer_url
 
     # Opens up youtube trailer in browser
     def show_trailer(self):
